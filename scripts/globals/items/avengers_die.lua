8e460da0fa04f8c098f830571efd2b3d8a4ba7ff
MrSent <mrsentientone@gmail.com>
2020-07-18 15:31:29 +0100

    Job abilities enum-ed
    Replaced cor die script magic numbers with the corresponding enums
    
    Added every job ability missing from the enum in ability.lua,
    these are just enums so they may or may not be in the sql yet.

--- scripts/globals/items/avengers_die.lua
+++ scripts/globals/items/avengers_die.lua
@@ -5,9 +5,9 @@
 -----------------------------------------
 
 function onItemCheck(target)
-    return target:canLearnAbility(289)
+    return target:canLearnAbility(tpz.jobAbility.AVENGER_S_ROLL)
 end
 
 function onItemUse(target)
-    target:addLearnedAbility(289)
+    target:addLearnedAbility(tpz.jobAbility.AVENGER_S_ROLL)
 end
--- scripts/globals/items/companions_die.lua
+++ scripts/globals/items/companions_die.lua
@@ -5,9 +5,9 @@
 -----------------------------------------
 
 function onItemCheck(target)
-    return target:canLearnAbility(288)
+    return target:canLearnAbility(tpz.jobAbility.COMPANION_S_ROLL)
 end
 
 function onItemUse(target)
-    target:addLearnedAbility(288)
+    target:addLearnedAbility(tpz.jobAbility.COMPANION_S_ROLL)
 end
